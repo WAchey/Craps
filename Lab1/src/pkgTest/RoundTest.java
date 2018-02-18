245f6c2cf02c5e8b5193f5fec705e8f1f82f2535
Omar Ahmad <eatmorerice@outlook.com>
2018-02-18 17:06:34 -0500

    Created roundTest (nothing in it yet)

---
+++ Lab1/src/pkgTest/RoundTest.java
@@ -0,0 +1,19 @@
+package pkgTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoundTest {
+
+	@Test
+	public void testRollCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRoundOver() {
+		fail("Not yet implemented");
+	}
+
+}
